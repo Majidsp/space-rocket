51e5f5158aa1d0e47ed0f165e3c8a7e066601ab0
Majid Sepandarand <majid_sp2007@yahoo.com>
2021-10-19 12:41:57 +0200

    Added luxon library and handled formating timestamp considering their timezomes with luxon library.

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,20 +1,29 @@
+import { DateTime } from "luxon";
+
 export function formatDate(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  }).format(new Date(timestamp));
+    return new Intl.DateTimeFormat("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }).format(new Date(timestamp));
 }
 
 export function formatDateTime(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZoneName: "short",
-  }).format(new Date(timestamp));
+    return new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZoneName: "short",
+    }).format(new Date(timestamp));
+}
+
+export function formatDateTimeWithTimeZone(timestamp, considerTimeZone) {
+    const keepOffset = DateTime.fromISO(timestamp, {
+        setZone: considerTimeZone,
+    }).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+    return keepOffset;
 }
