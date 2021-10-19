0137be16c85654ca0fc69bb92e336b597f866a58
Majid Sepandarand <majid_sp2007@yahoo.com>
2021-10-19 16:02:10 +0200

    Added and working on the Drawer component.

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex, Text, Box } from "@chakra-ui/core";
+import { Star } from "react-feather";
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+} from "@chakra-ui/core";
 
 import Launches from "./launches";
 import Launch from "./launch";
@@ -9,39 +20,76 @@ import LaunchPads from "./launch-pads";
 import LaunchPad from "./launch-pad";
 
 export default function App() {
-  return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/launches" element={<Launches />} />
-        <Route path="/launches/:launchId" element={<Launch />} />
-        <Route path="/launch-pads" element={<LaunchPads />} />
-        <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/launches" element={<Launches />} />
+                <Route path="/launches/:launchId" element={<Launch />} />
+                <Route path="/launch-pads" element={<LaunchPads />} />
+                <Route
+                    path="/launch-pads/:launchPadId"
+                    element={<LaunchPad />}
+                />
+            </Routes>
+        </div>
+    );
 }
 
 function NavBar() {
-  return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="6"
-      bg="gray.800"
-      color="white"
-    >
-      <Text
-        fontFamily="mono"
-        letterSpacing="2px"
-        fontWeight="bold"
-        fontSize="lg"
-      >
-        ¡SPACE·R0CKETS!
-      </Text>
-    </Flex>
-  );
+    return (
+        <Flex
+            as="nav"
+            align="center"
+            justify="space-between"
+            wrap="wrap"
+            padding="6"
+            bg="gray.800"
+            color="white"
+        >
+            <Text
+                fontFamily="mono"
+                letterSpacing="2px"
+                fontWeight="bold"
+                fontSize="lg"
+            >
+                ¡SPACE·R0CKETS!
+            </Text>
+            <SlideInDrawer />
+        </Flex>
+    );
+}
+
+function SlideInDrawer() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const menuRef = React.useRef();
+
+    return (
+        <>
+            <Box
+                as={Star}
+                width="6em"
+                color="white"
+                ref={menuRef}
+                onClick={onOpen}
+            />
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={menuRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Favorites</DrawerHeader>
+
+                    <DrawerBody></DrawerBody>
+
+                    <DrawerFooter></DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </>
+    );
 }
